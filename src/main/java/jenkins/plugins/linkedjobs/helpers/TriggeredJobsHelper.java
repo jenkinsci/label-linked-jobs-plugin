08aa46a8dd9c37df48142bc0e97b55f305b6ab65
Dominique <dominiquebrice@users.noreply.github.com>
2015-04-08 22:00:57 -0400

    Take into account another use case with NodeLabel Parameter Plugin

--- src/main/java/jenkins/plugins/linkedjobs/helpers/TriggeredJobsHelper.java
+++ src/main/java/jenkins/plugins/linkedjobs/helpers/TriggeredJobsHelper.java
@@ -1,9 +1,13 @@
 package jenkins.plugins.linkedjobs.helpers;
 
 import hudson.model.AbstractProject;
+import hudson.model.JobProperty;
 import hudson.model.Label;
+import hudson.model.ParameterDefinition;
+import hudson.model.ParametersDefinitionProperty;
 import hudson.model.Project;
 import hudson.plugins.parameterizedtrigger.AbstractBuildParameterFactory;
+import hudson.plugins.parameterizedtrigger.AbstractBuildParameters;
 import hudson.plugins.parameterizedtrigger.BlockableBuildTriggerConfig;
 import hudson.plugins.parameterizedtrigger.TriggerBuilder;
 import hudson.tasks.Builder;
@@ -14,7 +18,9 @@ import java.util.List;
 import jenkins.model.Jenkins;
 import jenkins.plugins.linkedjobs.model.TriggeredJob;
 
+import org.jvnet.jenkins.plugins.nodelabelparameter.LabelParameterDefinition;
 import org.jvnet.jenkins.plugins.nodelabelparameter.parameterizedtrigger.AllNodesForLabelBuildParameterFactory;
+import org.jvnet.jenkins.plugins.nodelabelparameter.parameterizedtrigger.NodeLabelBuildParameter;
 
 public class TriggeredJobsHelper {
 
@@ -31,6 +37,18 @@ public class TriggeredJobsHelper {
         
         for (AbstractProject<?, ?> triggeringJob : Jenkins.getInstance().getAllItems(
                 AbstractProject.class)) {
+            
+            for (JobProperty property : triggeringJob.getProperties().values()) {
+                if (property instanceof ParametersDefinitionProperty) {
+                    for (ParameterDefinition pdef : ((ParametersDefinitionProperty)property).getParameterDefinitions()) {
+                        if (pdef instanceof LabelParameterDefinition) {
+                            String defaultLabel = ((LabelParameterDefinition)pdef).defaultValue;
+                            // TODO: do something with these default labels?
+                        }
+                    }
+                }
+            }
+            
             if (!(triggeringJob instanceof Project)) {
                 continue;
             }
@@ -41,43 +59,62 @@ public class TriggeredJobsHelper {
                 // this job is triggering other jobs...
                 List<BlockableBuildTriggerConfig> configs = ((TriggerBuilder) builder).getConfigs();
                 for (BlockableBuildTriggerConfig config : configs) {
+                    
+                    // use case one: a Parameters section
+                    for (AbstractBuildParameters parameter : config.getConfigs()) {
+                        if (parameter instanceof NodeLabelBuildParameter) {
+                            // job is triggering other jobs based on specific label
+                            // using the nodelabelparameter plugin
+                            addTriggeredJob(triggeredJobsByLabel, ((NodeLabelBuildParameter)parameter).nodeLabel,
+                                    config, triggeringJob);
+                        }
+                    }
+                    
+                    // use case two: a ParameterFactories section
                     List<AbstractBuildParameterFactory> factories = config.getConfigFactories();
                     if (factories == null) {
                         continue;
                     }
+                    
                     for (AbstractBuildParameterFactory factory : factories) {
                         if (!(factory instanceof AllNodesForLabelBuildParameterFactory)) {
                             continue;
                         }
 
-                        // and it's triggering jobs based on specific label
+                        // job is triggering other jobs based on specific label
                         // using the nodelabelparameter plugin
-                        Label currentLabel = Jenkins.getInstance().getLabel(
-                            ((AllNodesForLabelBuildParameterFactory) factory).nodeLabel);
-                        // TODO: expand nodeLabel?
-
-                        HashMap<AbstractProject<?, ?>, TriggeredJob> jobsTriggeredByCurrentLabel = triggeredJobsByLabel.get(currentLabel);
-                        if (jobsTriggeredByCurrentLabel == null) {
-                            jobsTriggeredByCurrentLabel = new HashMap<AbstractProject<?,?>, TriggeredJob>();
-                            triggeredJobsByLabel.put(currentLabel, jobsTriggeredByCurrentLabel);
-                        }
-
-                        // get the list of all triggered jobs
-                        List<AbstractProject> triggeredJobs = config.getProjectList(null);
-                        if (triggeredJobs != null) {
-                            for (AbstractProject triggeredJob : triggeredJobs) {
-                                // and store them, associated to the triggering job and currentLabel
-                                TriggeredJob triggeredJobData = jobsTriggeredByCurrentLabel.get(triggeredJob);
-                                if (triggeredJobData == null) {
-                                    triggeredJobData = new TriggeredJob(triggeredJob, triggeringJob);
-                                    jobsTriggeredByCurrentLabel.put(triggeredJob, triggeredJobData);
-                                }
-                                else {
-                                    triggeredJobData.addTriggeringJob(triggeringJob);
-                                }
-                            }
-                        }
+                        addTriggeredJob(triggeredJobsByLabel, ((AllNodesForLabelBuildParameterFactory) factory).nodeLabel,
+                                config, triggeringJob);
                     }
+                }
+            }
+        }
+    }
+    
+    private static void addTriggeredJob(HashMap<Label, HashMap<AbstractProject<?,?>, TriggeredJob>> triggeredJobsByLabel,
+            String strLabel, BlockableBuildTriggerConfig config, AbstractProject<?, ?> triggeringJob) {
+        
+        // TODO: expand nodeLabel?
+        Label label = Jenkins.getInstance().getLabel(strLabel);
+        
+        HashMap<AbstractProject<?, ?>, TriggeredJob> jobsTriggeredByCurrentLabel = triggeredJobsByLabel.get(label);
+        if (jobsTriggeredByCurrentLabel == null) {
+            jobsTriggeredByCurrentLabel = new HashMap<AbstractProject<?,?>, TriggeredJob>();
+            triggeredJobsByLabel.put(label, jobsTriggeredByCurrentLabel);
+        }
+
+        // get the list of all triggered jobs
+        List<AbstractProject> triggeredJobs = config.getProjectList(null);
+        if (triggeredJobs != null) {
+            for (AbstractProject triggeredJob : triggeredJobs) {
+                // and store them, associated to the triggering job and currentLabel
+                TriggeredJob triggeredJobData = jobsTriggeredByCurrentLabel.get(triggeredJob);
+                if (triggeredJobData == null) {
+                    triggeredJobData = new TriggeredJob(triggeredJob, triggeringJob);
+                    jobsTriggeredByCurrentLabel.put(triggeredJob, triggeredJobData);
+                }
+                else {
+                    triggeredJobData.addTriggeringJob(triggeringJob);
                 }
             }
         }
